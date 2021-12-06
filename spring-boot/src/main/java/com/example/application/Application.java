fbb66db1d5c498ae735a2a5dc775aae749f990d7
KolesnykMA <maxymura@gmail.com>
2021-12-06 22:07:01 +0200

    feat: Add another custom starter for testing

--- spring-boot/src/main/java/com/example/application/Application.java
+++ spring-boot/src/main/java/com/example/application/Application.java
@@ -2,8 +2,10 @@ package com.example.application;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class Application {
--- spring-boot/src/main/java/com/example/application/controller/TeacherController.java
+++ spring-boot/src/main/java/com/example/application/controller/TeacherController.java
@@ -3,20 +3,27 @@ package com.example.application.controller;
 import com.example.application.dto.TeacherDTO;
 import com.example.application.model.Teacher;
 import com.example.application.service.TeacherService;
+import maks.starter.mycustomstarter.HelloService;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 import java.util.stream.Collectors;
 
 /**
  * Constructor Injection
  */
+@Configuration
 @RestController
 public class TeacherController {
     TeacherService teacherService;
     ModelMapper modelMapper;
+
+    @Autowired
+    HelloService helloService;
+
 
     public TeacherController(TeacherService teacherService, ModelMapper modelMapper) {
         this.teacherService = teacherService;
@@ -25,6 +32,7 @@ public class TeacherController {
 
     @GetMapping("/teachers")
     public List<TeacherDTO> getAllStudents() {
+        helloService.hello();
 
         List<Teacher> teachers = teacherService.getAllTeachers();
 
