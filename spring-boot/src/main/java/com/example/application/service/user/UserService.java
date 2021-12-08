c72ea43b7f9d9089b027c04a4e6cafffd08d1f49
Xymeterid <gleb.savkin.kiev@gmail.com>
2021-12-08 18:11:48 +0200

    Added caching

--- spring-boot/src/main/java/com/example/application/service/user/UserService.java
+++ spring-boot/src/main/java/com/example/application/service/user/UserService.java
@@ -3,6 +3,8 @@ package com.example.application.service.user;
 import com.example.application.dal.model.User;
 import com.example.application.dal.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,10 +22,12 @@ public class UserService {
         return userRepository.getUsersByRole(role);
     }
 
+    @Cacheable("users")
     public Optional<User> getUser(int id) {
         return userRepository.findById(id);
     }
 
+    @CacheEvict(value="users", allEntries=true)
     public void updateUser(User user) { userRepository.save(user); }
 
     public void deleteUser(int id) { userRepository.deleteById(id); }
